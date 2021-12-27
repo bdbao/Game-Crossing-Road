e68bcf745ede37c2e4daa95f1802fe5345b8e67d
hientoanlam <73538471+lamhientoan@users.noreply.github.com>
2021-12-27 16:50:33 +0700

    define CSETTINGS

---
+++ Crossing Road/CSETTINGS.h
@@ -0,0 +1,24 @@
+#ifndef CSETTINGS_H
+#define CSETTINGS_H
+
+#include "CCONSTANT.h"
+#include <iostream>
+
+class  CSETTINGS {
+public:
+	CSETTINGS();
+	~CSETTINGS();
+
+	void draw(sf::RenderWindow& window);
+	int getOption();
+
+	void moveUp();
+	void moveDown();
+private:
+	int selectedOption = 0;
+	sf::Texture title;
+	sf::Font font;
+	sf::Text* settings = new sf::Text[CCONSTANT::NUMBER_OF_SETTING_OPTIONS];
+};
+
+#endif // !CSETTINGS_H
